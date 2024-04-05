921d4fb53fb8788cb35c4851a47b01c6751b89fe
Jcwscience <jcwscience@me.com>
2024-04-04 18:25:19 -0600

    Removed broken app.py

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
 from translator import Translator
 from xtts_live.xtts_live import TextToSpeech
-import sounddevice as sd  # Import the sounddevice module for audio streaming
-
-
-
-model_path = "/home/john/Documents/XTTS-v2/"  # Path to the model directory
-speaker_wavs = "/home/john/Documents/Voices/voice.wav"  # Path to the speaker's audio file
-
-output_samplerate = 48000  # Output audio sample rate
-
-tts = TextToSpeech(model_path, speaker_wavs, output_samplerate, use_deepspeed=True, debug=False)
-
-def stream_callback(outdata, frames, time, status):
-    if status:
-        print(status)  # Print any status messages received during audio streaming
-    outdata[:] = tts.audio_buffer.get_samples(frames)  # Fill the output audio buffer with samples from the TextToSpeech instance
-
-
-stream = sd.OutputStream(
-    device=20,  # Specify the audio output device index
-    samplerate=output_samplerate,  # Set the sample rate for audio streaming
-    channels=1,  # Set the number of audio channels
-    callback=stream_callback,  # Set the callback function for audio streaming
-    finished_callback=tts.stop,  # Set the finished callback function to stop the TextToSpeech instance
-    dtype='float32'  # Set the data type for audio samples
-)
-
-stream.start()  # Start the audio streaming
+import sounddevice as sd
 
 
 # Initialize the Translator and TextToSpeechSystem
-translator = Translator()  # Assuming English to Japanese translation as default
+translator = Translator()
 
+model_path = "/home/john/Documents/XTTS-v2/"  # Path to the model directory
+speaker_wavs = "/home/john/Documents/Voices/voice.wav"  # Path to the speaker's audio file
+output_samplerate = 48000  # Output audio sample rate
+
+tts = TextToSpeech(model_path, speaker_wavs, output_samplerate, use_deepspeed=True, debug=False)
 supported_languages = tts.supported_languages
+
+def stream_callback(outdata, frames, time, status):
+    outdata[:] = tts.audio_buffer.get_samples(frames)
+
+stream = sd.OutputStream(
+    device=20,  # Specify the audio output device index
+    samplerate=output_samplerate,
+    channels=1,
+    callback=stream_callback,
+    dtype='float32'
+)
+
+stream.start()  
 
 while True:
     try:
